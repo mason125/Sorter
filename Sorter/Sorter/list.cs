50bd41bd7f7eeb5aba0792233b65fb3416a5fb27
mason125 <mason.white@snhu.edu>
2018-11-21 21:25:48 -0600

    list bubble sort added
    added bubble sort for list

---
+++ Sorter/Sorter/list.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace Sorter
+{
+    namespace ListSort
+    {
+        //this class handles all the sorting for Lists generic type
+        public class list : sort
+        {
+            public static List<T> bubble<T>(List<T> data)
+            {
+
+                var al = new ArrayList();
+                foreach (dynamic li in data)
+                    al.Add(li);
+
+
+                //sort
+                var sorted = sort.bubble(al);
+
+
+                //convert back to list
+                var cl = new List<T>();
+                foreach (dynamic li in sorted)
+                    cl.Add(li);
+
+                return cl;
+            }
+            
+
+        }
+    }
+}
---
+++ Sorter/Sorter/sort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace Sorter
+{
+    //this is the sorting class all the sorting for each 
+    //collection type is done here
+    public class sort
+    {
+        protected static ArrayList bubble(ArrayList data)
+        {
+            int count = data.Count;
+
+
+            for (int i = 0; i < count - 1; ++i)
+            {
+                for (int j = 0; j < count - i - 1; ++j)
+                {
+                    dynamic holder1 = data[j];
+                    dynamic holder2 = data[j + 1];
+
+                    if (holder1 > holder2)
+                    {
+                        dynamic temp = data[j];
+                        data[j] = data[j + 1];
+                        data[j + 1] = temp;
+                    }
+                }
+            }
+
+            return data;
+        }
+    }
+}
